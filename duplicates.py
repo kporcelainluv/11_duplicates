43edb79c4853d19abd488cac768551690efedf9d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2017-12-12 14:55:05 +0300

    simplifies code, added new defs

--- duplicates.py
+++ duplicates.py
@@ -1,32 +1,33 @@
+from collections import defaultdict
 import os
 import sys
 
 
 def collecting_filenames_from_dir(directory_path):
-    dict_of_filenames_in_dir = {}
-    dictionary_of_duplicates = {}
+    dict_of_filenames_in_dir = defaultdict(list)
     for path, dirs, files_names in os.walk(directory_path):
         for filename in files_names:
             size_of_file = str(os.stat(os.path.join(path, filename)).st_size)
             name_and_size_of_file = (filename, size_of_file)
-            if name_and_size_of_file in dict_of_filenames_in_dir:
-                dict_of_filenames_in_dir[name_and_size_of_file].append(path)
-                if len(dict_of_filenames_in_dir[name_and_size_of_file]) >= 2:
-                    dictionary_of_duplicates[name_and_size_of_file] = dict_of_filenames_in_dir[name_and_size_of_file]
-            else:
-                dict_of_filenames_in_dir[name_and_size_of_file] = [path]
-    return dictionary_of_duplicates
+            dict_of_filenames_in_dir[name_and_size_of_file].append(path)
+    return dict_of_filenames_in_dir.items()
 
 
-def finding_duplicates(dictionary_of_duplicates):
-    print("The duplicates are: ")
+def finding_duplicates(dict_of_filenames_in_dir):
+    dictionary_of_duplicates = {}
+    for filename_and_path in dict_of_filenames_in_dir:
+        name_and_size_of_file = filename_and_path[0]
+        paths = filename_and_path[1]
+        if len(paths) >= 2:
+            dictionary_of_duplicates[name_and_size_of_file] = paths
     for name_and_size in dictionary_of_duplicates:
         print("filename:", name_and_size[0], end="\n")
         for path_to_file in dictionary_of_duplicates[name_and_size]:
-            print("path to file:", (os.getcwd() + "/" + path_to_file))
+            print("path to file:", os.path.join(os.getcwd() + "/" + path_to_file))
     return "Thank you for using the program!"
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        print("The duplicates are: ")
         print(finding_duplicates(collecting_filenames_from_dir(sys.argv[1])))
